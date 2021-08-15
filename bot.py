9bcd4dc35ac0dec06b2b55eb233defafd0117efd
aleksrovinski <nesegsasa@gmail.com>
2021-08-15 20:26:27 +0300

    Загатовка раздела "Школа"

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 #импорт библиотек
+from typing import Text
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.types import message
@@ -11,11 +12,17 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=["start"])
 async def start(message: types.Message):
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
     buttons1 = ["Школа", "Обратная связь", "Канал", "Другое", "Другие проекты"]
     keyboard.add(*buttons1)
     await message.reply("Привет", reply_markup=keyboard)
 
+@dp.message_handler(lambda message: message.text == "Школа")
+async def school(message: types.Message):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    buttons = ['Расписание', 'ДЗ', 'Назад']
+    keyboard.add(*buttons)
+    await message.answer('Раздел "Школа"', reply_markup=keyboard)
 
 if __name__ == '__main__':
     executor.start_polling(dp)
