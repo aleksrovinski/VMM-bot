e9427791c13975efd443d809f8edbfdbb784e464
aleksrovinski <nesegsasa@gmail.com>
2021-08-16 11:19:29 +0300

    Задел на обратную связь

--- bot.py
+++ bot.py
@@ -22,20 +22,29 @@ async def school(message: types.Message):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     buttons = ['Расписание', 'ДЗ', 'Назад']
     keyboard.add(*buttons)
-    await message.answer('Раздел "Школа"', reply_markup=keyboard)
+    await message.reply('Раздел "Школа"', reply_markup=keyboard)
 @dp.message_handler(lambda message: message.text == "ДЗ")
 async def schooldz(message: types.Message):
-    await message.answer("функция отключена до 1.09.2021")
+    await message.reply("функция отключена до 1.09.2021")
 @dp.message_handler(lambda message: message.text == "Расписание")
 async def schoollessons(message: types.Message):
-    await message.answer("функция отключена до 1.09.2021")
+    await message.reply("функция отключена до 1.09.2021")
 @dp.message_handler(lambda message: message.text == "Назад")
 async def back(message: types.Message):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
     buttons1 = ["Школа", "Обратная связь", "Канал", "Другое", "Другие проекты"]
     keyboard.add(*buttons1)
-    await message.answer("Главное меню", reply_markup=keyboard)
-
+    await message.reply("Главное меню", reply_markup=keyboard)
+@dp.message_handler(lambda message: message.text == "Обратная связь")
+async def feedback(message: types.Message):
+    buttons = [
+        types.InlineKeyboardButton(text="Разработчик", url="t.me/aleksrovi"),
+        types.InlineKeyboardButton(text="Github", url="https://github.com/aleksrovinski/VMM-bot/issues"),
+        types.InlineKeyboardButton(text="Прям тут", callback_data="feedback1")
+    ]
+    keyboard = types.InlineKeyboardMarkup(row_width=1)
+    keyboard.add(*buttons)
+    await message.reply("Выбири вариант обратной связи:", reply_markup=keyboard)
 
 
 
