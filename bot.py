aefd7ea01942244d18fd4d559cb127e5119afd0b
aleksrovinski <nesegsasa@gmail.com>
2021-08-16 10:20:40 +0300

    Доработка раздела "Школа"

--- bot.py
+++ bot.py
@@ -23,6 +23,21 @@ async def school(message: types.Message):
     buttons = ['Расписание', 'ДЗ', 'Назад']
     keyboard.add(*buttons)
     await message.answer('Раздел "Школа"', reply_markup=keyboard)
+@dp.message_handler(lambda message: message.text == "ДЗ")
+async def schooldz(message: types.Message):
+    await message.answer("функция отключена до 1.09.2021")
+@dp.message_handler(lambda message: message.text == "Расписание")
+async def schoollessons(message: types.Message):
+    await message.answer("функция отключена до 1.09.2021")
+@dp.message_handler(lambda message: message.text == "Назад")
+async def back(message: types.Message):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    buttons1 = ["Школа", "Обратная связь", "Канал", "Другое", "Другие проекты"]
+    keyboard.add(*buttons1)
+    await message.answer("Главное меню", reply_markup=keyboard)
+
+
+
 
 if __name__ == '__main__':
     executor.start_polling(dp)
